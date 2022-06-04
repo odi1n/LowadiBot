d71e37f9f377979f97bef60dd0a7d602ad0c7605
odi1n <sashf1999@mail.ru>
2022-06-04 14:27:00 +0300

    Add driver page

---
+++ LowadiBot/Views/Pages/DriverPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace LowadiBot.Views.Pages
+{
+    public partial class DriverPage : Page
+    {
+        public DriverPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
